5b60fce940a380922f74692b5477448a66a92071
Yuriy <suhyura@gmail.com>
2018-10-06 11:09:30 +0200

    dispaly simple articles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+interface Product{
+  name: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'devmeetings-angular';
+  products: Product[]=[
+    {name:'apple'},
+    {name:'paple'},
+    {name:'maple'}];
 }
